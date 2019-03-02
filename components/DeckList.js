caed7e32f386ff08171b658455eb011e5f783013
Nour Sabouny <nour.sabouny@tigerspike.com>
2019-03-02 21:29:07 +0400

    feat: Styling DeckList view component

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,53 +1,63 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+	FlatList
+} from "react-native";
 import { connect } from "react-redux";
+import Icon from "react-native-vector-icons/Ionicons";
 
 class DeckList extends Component {
+	renderDeckItem = ({ item: deck, index }) => (
+		<TouchableOpacity
+			onPress={() =>
+				this.props.navigation.navigate("DeckView", {
+					deckId: deck.id
+				})
+			}
+			style={styles.deck}
+		>
+			<Icon name="md-card" style={styles.icon} size={60} color="#900" />
+			<View>
+				<Text style={styles.deckTitle}>{deck.title}</Text>
+				<Text style={styles.deckSubtitle}>
+					{`Cards: ${deck.questions ? deck.questions.length : 0}`}
+				</Text>
+			</View>
+		</TouchableOpacity>
+	);
+
 	render() {
 		let { decks } = this.props;
-		let components = Object.keys(decks).map(key => {
-			const deck = decks[key];
-			return (
-				<TouchableOpacity
-					onPress={() =>
-						this.props.navigation.navigate("DeckView", {
-							deckId: key
-						})
-					}
-					style={styles.deck}
-					key={`${key}_button`}
-				>
-					<View key={key}>
-						<Text style={styles.deckTitle}>{deck.title}</Text>
-						<Text style={styles.deckSubtitle}>
-							{`Cards: ${
-								deck.questions ? deck.questions.length : 0
-							}`}
-						</Text>
-					</View>
-				</TouchableOpacity>
-			);
-		});
-		return <View style={styles.container}>{components}</View>;
+		let components = Object.keys(decks).map(key => decks[key]);
+		return (
+			<FlatList
+				renderItem={this.renderDeckItem}
+				data={components}
+				style={styles.container}
+				keyExtractor={(item, index) => item.id}
+			/>
+		);
 	}
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: "#eee",
-		alignItems: "center",
-		justifyContent: "center"
+		flex: 1
 	},
 	deck: {
 		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "flex-start",
-		justifyContent: "center",
+		alignItems: "center",
+		justifyContent: "flex-start",
 		alignSelf: "stretch",
+		flexDirection: "row",
+		backgroundColor: "#fff",
 		borderBottomColor: "#eee",
 		borderBottomWidth: 1,
-		paddingLeft: 30
+		paddingLeft: 30,
+		paddingVertical: 20
 	},
 	deckTitle: {
 		fontSize: 30,
@@ -57,9 +67,14 @@ const styles = StyleSheet.create({
 		justifyContent: "center"
 	},
 	deckSubtitle: {
-		color: "#000",
+		color: "#D81B37",
 		alignItems: "center",
 		justifyContent: "center"
+	},
+	icon: {
+		alignSelf: "stretch",
+		paddingTop: 4,
+		marginRight: 15
 	}
 });
 
